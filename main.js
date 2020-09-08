b0dd6ac82810c4222c6acc117124d49630d4add0
Timemaster111 <53912018+Timemaster111@users.noreply.github.com>
2020-09-08 19:08:19 +0100

    Initial Bot

---
+++ main.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+client.on('ready', ()=>{
+    client.user.setActivity("for updates!", {type:"WATCHING"})
+    console.log('logged in to '+client.guilds.size+' servers')
+})
+client.on('message', message=>{
+    if(message.author.bot) return
+    let content = message.content
+    if(!content.startsWith('!')) return
+    content = content.substring(1)
+    args = content.split(/ +/)
+    command = args.shift()
+    switch(command.toLowerCase()){
+        case "birthday":
+            if(args.length != 2) return message.reply('Please give me a single user to ping and mention a channel.')
+            user = message.mentions.members.first()
+            if(!user) user = message.guild.members.find(val => val.id == args[0]||val.user.username == args[0] || val.nickname == args[0])
+            if(!user) return message.reply('Invalid user.')
+            channel = message.mentions.channels.first()
+            if(!channel) channel = message.guild.channels.find(val => val.id == args[1] || val.name == args[1])
+            if(!channel) return message.reply('Invalid channel.')
+            channel.send(`Happy Birthday ${user}!`)
+            
+    }
+})
+client.login(process.env.TOKEN)
